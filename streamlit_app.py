b917bac9dbd7070f68500d20d69e1f6536e403d0
sudhan-dhanaraj <119043315+sudhan-dhanaraj@users.noreply.github.com>
2022-11-25 15:28:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
+import pandas
 
-streamlit.title('My First Streamlit App')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.tile('Pandas Dataframe')
+df = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(df)
