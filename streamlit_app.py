4e99988b98dc7b78d55ffc6a62e818de4e02c89c
sudhan-dhanaraj <119043315+sudhan-dhanaraj@users.noreply.github.com>
2022-11-25 15:34:49 +0530

    Update streamlit_app.py
    Prepopulating pick list

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('Pandas Dataframe')
 df = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 df = df.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(df.index))
+streamlit.multiselect("Pick some fruits:", list(df.index),['Apple','Grapefruit'])
 streamlit.dataframe(df)
