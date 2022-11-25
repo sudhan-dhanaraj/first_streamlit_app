01acfb06101ba7c73f51f8ad4e9372c8700faca7
sudhan-dhanaraj <119043315+sudhan-dhanaraj@users.noreply.github.com>
2022-11-25 15:30:17 +0530

    Update streamlit_app.py
    Added multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,6 @@ import pandas
 
 streamlit.title('Pandas Dataframe')
 df = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(df.index))
 streamlit.dataframe(df)
