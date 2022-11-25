14067e402640e499a4753890cacb164ed5907005
sudhan-dhanaraj <119043315+sudhan-dhanaraj@users.noreply.github.com>
2022-11-25 15:28:57 +0530

    Update streamlit_app.py
    Added Pandas and fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas
 
-streamlit.tile('Pandas Dataframe')
+streamlit.title('Pandas Dataframe')
 df = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(df)
